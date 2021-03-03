658f2cf8e017b94f8cb89f6427a91dee51661d84
Antonius Hegyes <a.hegyes@deep-web-solutions.de>
2021-03-03 16:42:34 +0100

    Fixed more code smells

--- src/includes/Dependencies/Handlers/WPPluginsHandler.php
+++ src/includes/Dependencies/Handlers/WPPluginsHandler.php
@@ -46,41 +46,19 @@ class WPPluginsHandler extends AbstractDependenciesHandler {
 	 * @since   1.0.0
 	 * @version 1.0.0
 	 *
-	 * @SuppressWarnings(PHPMD.CyclomaticComplexity)
-	 *
 	 * @return  array
 	 */
 	public function get_missing_dependencies(): array {
 		$missing = array();
 
 		foreach ( $this->get_dependencies() as $active_plugin => $active_plugin_config ) {
-			if ( isset( $active_plugin_config['active_checker'] ) && is_callable( $active_plugin_config['active_checker'] ) ) {
-				$is_active = boolval( call_user_func( $active_plugin_config['active_checker'] ) );
-			} else {
-				$is_active = in_array( $active_plugin, apply_filters( 'active_plugins', get_option( 'active_plugins' ) ), true ); // phpcs:ignore
-				if ( is_multisite() && ! $is_active ) {
-					$is_active = isset( get_site_option( 'active_sitewide_plugins', array() )[ $active_plugin ] );
-				}
-			}
+			$is_active = $this->is_plugin_active( $active_plugin, $active_plugin_config );
 
 			if ( ! $is_active ) {
 				$missing[ $active_plugin ] = $active_plugin_config;
 			} elseif ( isset( $active_plugin_config['min_version'] ) ) {
-				if ( isset( $active_plugin_config['version_checker'] ) && is_callable( $active_plugin_config['version_checker'] ) ) {
-					$version = call_user_func( $active_plugin_config['version_checker'] );
-				} else {
-					$wp_filesystem = $this->get_wp_filesystem();
-					$version       = '0.0.0';
-
-					if ( $wp_filesystem ) {
-						$plugin_data = get_file_data( trailingslashit( $wp_filesystem->wp_plugins_dir() ) . $active_plugin, array( 'Version' => 'Version' ) );
-						if ( isset( $plugin_data['Version'] ) ) {
-							$version = $plugin_data['Version'];
-						}
-					}
-				}
-
-				if ( version_compare( $active_plugin_config['min_version'], $version, '>' ) ) {
+				$version = $this->get_active_plugin_version( $active_plugin, $active_plugin_config );
+				if ( version_compare( $version, $active_plugin_config['min_version'], '<' ) ) {
 					$missing[ $active_plugin ] = $active_plugin_config + array( 'version' => $version );
 				}
 			}
@@ -107,6 +85,59 @@ class WPPluginsHandler extends AbstractDependenciesHandler {
 		return is_array( $dependency ) && Arrays::has_string_keys( $dependency )
 			? $dependency
 			: null;
+	}
+
+	/**
+	 * Returns whether a given plugin is active or not.
+	 *
+	 * @since   1.0.0
+	 * @version 1.0.0
+	 *
+	 * @param   string  $plugin             Plugin to check.
+	 * @param   array   $plugin_config      Dependency configuration.
+	 *
+	 * @return  bool
+	 */
+	protected function is_plugin_active( string $plugin, array $plugin_config ): bool {
+		if ( isset( $plugin_config['active_checker'] ) && is_callable( $plugin_config['active_checker'] ) ) {
+			$is_active = boolval( call_user_func( $plugin_config['active_checker'] ) );
+		} else {
+			$is_active = in_array( $plugin, apply_filters( 'active_plugins', get_option( 'active_plugins' ) ), true ); // phpcs:ignore
+			if ( is_multisite() && ! $is_active ) {
+				$is_active = isset( get_site_option( 'active_sitewide_plugins', array() )[ $plugin ] );
+			}
+		}
+
+		return $is_active;
+	}
+
+	/**
+	 * Returns the version of an installed and active plugin.
+	 *
+	 * @since   1.0.0
+	 * @version 1.0.0
+	 *
+	 * @param   string  $plugin             Plugin to check.
+	 * @param   array   $plugin_config      Dependency configuration.
+	 *
+	 * @return  string
+	 */
+	protected function get_active_plugin_version( string $plugin, array $plugin_config ): string {
+		if ( isset( $plugin_config['version_checker'] ) && is_callable( $plugin_config['version_checker'] ) ) {
+			$version = call_user_func( $plugin_config['version_checker'] );
+		} else {
+			$wp_filesystem = $this->get_wp_filesystem();
+			$version       = '0.0.0';
+
+			if ( $wp_filesystem ) {
+				$plugin_data = get_file_data( trailingslashit( $wp_filesystem->wp_plugins_dir() ) . $plugin, array( 'Version' => 'Version' ) );
+				if ( isset( $plugin_data['Version'] ) ) {
+					$version = $plugin_data['Version'];
+				}
+			}
+		}
+
+		return $version;
 	}
 
 	// endregion

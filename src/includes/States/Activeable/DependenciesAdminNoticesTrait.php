546bf8f488f1e490c56eec9cbed11a4477306462
Antonius Hegyes <a.hegyes@deep-web-solutions.de>
2021-03-03 16:35:01 +0100

    Fixed some code smells

--- src/includes/States/Activeable/DependenciesAdminNoticesTrait.php
+++ src/includes/States/Activeable/DependenciesAdminNoticesTrait.php
@@ -65,8 +65,6 @@ trait DependenciesAdminNoticesTrait {
 	 * @since   1.0.0
 	 * @version 1.0.0
 	 *
-	 * @SuppressWarnings(PHPMD.CyclomaticComplexity)
-	 *
 	 * @param   AdminNoticesService             $notices_service        Instance of the admin notices service.
 	 * @param   DependenciesCheckerInterface    $dependencies_checker   Instance of dependencies checker.
 	 *
@@ -82,33 +80,55 @@ trait DependenciesAdminNoticesTrait {
 			foreach ( $handler_dependencies as $handler => $dependencies ) {
 				$is_optional_handler = ( strpos( $handler, 'optional' ) !== false );
 				$store               = $is_optional_handler ? 'options' : 'dynamic';
-				$notice_handle       = $this->get_admin_notice_handle( "missing-{$type}", array( md5( wp_json_encode( $dependencies ) ) ) );
-				$notice_params       = array( 'capability' => 'activate_plugins' );
 
-				switch ( $type ) {
-					case 'php_extensions':
-						$message = $this->compose_missing_php_extensions_message( $dependencies, $is_optional_handler );
-						break;
-					case 'php_functions':
-						$message = $this->compose_missing_php_functions_message( $dependencies, $is_optional_handler );
-						break;
-					case 'php_settings':
-						$message = $this->compose_incompatible_php_settings_message( $dependencies, $is_optional_handler );
-						break;
-					case 'active_plugins':
-						$message = $this->compose_missing_plugins_message( $dependencies, $is_optional_handler );
-						break;
-					default:
-						throw new NotImplementedException( 'Dependencies admin notices scenario not supported' );
-				}
+				$notice_handle  = $this->get_admin_notice_handle( "missing-{$type}", array( md5( wp_json_encode( $dependencies ) ) ) );
+				$notice_message = $this->compose_message( $type, $dependencies, $is_optional_handler );
+				$notice_params  = array( 'capability' => 'activate_plugins' );
 
 				$notice = $is_optional_handler
-					? new DismissibleNotice( $notice_handle, $message, $notice_params )
-					: new Notice( $notice_handle, $message, $notice_params );
+					? new DismissibleNotice( $notice_handle, $notice_message, $notice_params )
+					: new Notice( $notice_handle, $notice_message, $notice_params );
 
 				$notices_service->add_notice( $notice, $store );
 			}
 		}
+	}
+
+	/**
+	 * Returns the admin notice message to display for the missing dependencies.
+	 *
+	 * @since   1.0.0
+	 * @version 1.0.0
+	 *
+	 * @SuppressWarnings(PHPMD.BooleanArgumentFlag)
+	 *
+	 * @param   string  $type               Message type to compose.
+	 * @param   array   $dependencies       The list of missing dependencies.
+	 * @param   bool    $is_optional        Whether the dependencies are optional or not.
+	 *
+	 * @throws  NotImplementedException     Thrown when the message type is not supported.
+	 *
+	 * @return  string
+	 */
+	protected function compose_message( string $type, array $dependencies, bool $is_optional = false ): string {
+		switch ( $type ) {
+			case 'php_extensions':
+				$message = $this->compose_missing_php_extensions_message( $dependencies, $is_optional );
+				break;
+			case 'php_functions':
+				$message = $this->compose_missing_php_functions_message( $dependencies, $is_optional );
+				break;
+			case 'php_settings':
+				$message = $this->compose_incompatible_php_settings_message( $dependencies, $is_optional );
+				break;
+			case 'active_plugins':
+				$message = $this->compose_missing_plugins_message( $dependencies, $is_optional );
+				break;
+			default:
+				throw new NotImplementedException( 'Dependencies admin notices type not supported' );
+		}
+
+		return $message;
 	}
 
 	/**

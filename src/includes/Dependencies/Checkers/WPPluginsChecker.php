9ad8a024289afb6ce5104dfc28c16a5033ebec8b
Antonius Hegyes <a.hegyes@deep-web-solutions.com>
2021-11-02 16:12:28 +0100

    enhanced plugins checker

--- src/includes/Dependencies/Checkers/WPPluginsChecker.php
+++ src/includes/Dependencies/Checkers/WPPluginsChecker.php
@@ -4,6 +4,7 @@ namespace DeepWebSolutions\Framework\Utilities\Dependencies\Checkers;
 
 use DeepWebSolutions\Framework\Helpers\DataTypes\Arrays;
 use DeepWebSolutions\Framework\Helpers\DataTypes\Booleans;
+use DeepWebSolutions\Framework\Helpers\DataTypes\Strings;
 use DeepWebSolutions\Framework\Helpers\FileSystem\FilesystemAwareTrait;
 use DeepWebSolutions\Framework\Utilities\Dependencies\AbstractDependenciesChecker;
 
@@ -38,7 +39,23 @@ class WPPluginsChecker extends AbstractDependenciesChecker {
 
 	// endregion
 
-	// region METHODS
+	// region INHERITED METHODS
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @since   1.0.0
+	 * @version 1.0.0
+	 */
+	public function register_dependency( $dependency ): bool {
+		$validity = $this->is_dependency_valid( $dependency );
+		if ( true === $validity ) {
+			$dependency['name']   = $this->get_plugin_name( $dependency['plugin'], $dependency );
+			$this->dependencies[] = $dependency;
+		}
+
+		return $validity;
+	}
 
 	/**
 	 * {@inheritDoc}
@@ -128,16 +145,67 @@ class WPPluginsChecker extends AbstractDependenciesChecker {
 		if ( isset( $plugin_config['version_checker'] ) && \is_callable( $plugin_config['version_checker'] ) ) {
 			$version = \call_user_func( $plugin_config['version_checker'] );
 		} else {
-			$wp_filesystem = $this->get_wp_filesystem();
-			$version       = '0.0.0';
-
-			if ( ! \is_null( $wp_filesystem ) ) {
-				$plugin_data = \get_file_data( \trailingslashit( $wp_filesystem->wp_plugins_dir() ) . $plugin, array( 'Version' => 'Version' ) );
-				$version     = $plugin_data['Version'] ?? $version;
-			}
+			$plugin_data = $this->get_plugin_data( $plugin, array( 'version' => 'Version' ) );
+			$version     = \is_null( $plugin_data ) ? '0.0.0' : ( $plugin_data['version'] ?: '0.0.0' );
 		}
 
 		return $version;
+	}
+
+	/**
+	 * Returns a plugin's name.
+	 *
+	 * @since   1.0.0
+	 * @version 1.0.0
+	 *
+	 * @param   string  $plugin             Plugin to check.
+	 * @param   array   $plugin_config      Dependency configuration.
+	 *
+	 * @return  string
+	 */
+	protected function get_plugin_name( string $plugin, array $plugin_config ): string {
+		if ( $this->is_plugin_active( $plugin, $plugin_config ) ) {
+			if ( isset( $plugin_config['name_getter'] ) && \is_callable( $plugin_config['name_getter'] ) ) {
+				$plugin_name = Strings::maybe_cast( \call_user_func( $plugin_config['name_getter'] ), '' );
+			} else {
+				$plugin_data = $this->get_plugin_data(
+					$plugin,
+					array(
+						'name'        => 'Plugin Name',
+						'text_domain' => 'Text Domain',
+					)
+				);
+				if ( ! \is_null( $plugin_data ) ) {
+					$plugin_name = $plugin_data['name'] ?: ( $plugin_config['fallback_name'] ?? '' );
+					if ( ! empty( $plugin_data['text_domain'] ) ) {
+						$plugin_name = \translate( $plugin_name, $plugin_data['text_domain'] ); // phpcs:ignore WordPress.WP.I18n
+					}
+				} else {
+					$plugin_name = $plugin_config['fallback_name'] ?? '';
+				}
+			}
+		} else {
+			$plugin_name = $plugin_config['fallback_name'] ?? '';
+		}
+
+		return $plugin_name;
+	}
+
+	/**
+	 * Retrieves header data from a given plugin's file.
+	 *
+	 * @since   1.0.0
+	 * @version 1.0.0
+	 *
+	 * @param   string  $plugin     The active plugin.
+	 * @param   array   $headers    The headers to retrieve.
+	 *
+	 * @return  array|null
+	 */
+	protected function get_plugin_data( string $plugin, array $headers ): ?array {
+		$wp_filesystem = $this->get_wp_filesystem();
+		return \is_null( $wp_filesystem ) ? null
+			: \get_file_data( \trailingslashit( $wp_filesystem->wp_plugins_dir() ) . $plugin, $headers );
 	}
 
 	// endregion
